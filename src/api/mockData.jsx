f27f7fc6c5294c23509b02357292c29b0b47f4fe
Luka Jurić-Grgić <luka.juricgrgic@q.agency>
2024-02-15 16:44:20 +0100

    mockdata.jsx (mock data for destinations)

---
+++ src/api/mockData.jsx
@@ -0,0 +1,80 @@
+export const destinations = [
+  {
+    city: "Tokyo",
+    country: "Japan",
+    shortDescription:
+      "Immerse yourself in the vibrant culture of Tokyo, the Land of the Rising Sun. Experience the perfect blend of traditional and modern attractions, from ancient temples to high-tech districts.",
+    rating: 4.8,
+    price: {
+      amount: 1500,
+      currency: "¥",
+    },
+    imageUrl: "https://source.unsplash.com/800x600/?tokyo",
+    category: "Adventure",
+  },
+  {
+    city: "Rome",
+    country: "Italy",
+    shortDescription:
+      "Explore the historic charm of Rome, the Eternal City. Wander through ancient ruins, marvel at Renaissance art, and indulge in delicious Italian cuisine.",
+    rating: 4.6,
+    price: {
+      amount: 1600,
+      currency: "€",
+    },
+    imageUrl: "https://source.unsplash.com/800x600/?rome",
+    category: "Cultural",
+  },
+  {
+    city: "Barcelona",
+    country: "Spain",
+    shortDescription:
+      "Indulge in the architectural wonders of Barcelona, including Gaudi's masterpieces. Immerse yourself in the rich culture, vibrant street life, and delicious tapas.",
+    rating: 4.6,
+    price: {
+      amount: 1300,
+      currency: "€",
+    },
+    imageUrl: "https://source.unsplash.com/800x600/?barcelona",
+    category: "Cultural",
+  },
+  {
+    city: "Bangkok",
+    country: "Thailand",
+    shortDescription:
+      "Experience the bustling metropolis of Bangkok, the City of Angels. Discover ancient temples, vibrant markets, and tantalizing street food.",
+    rating: 4.7,
+    price: {
+      amount: 1200,
+      currency: "฿",
+    },
+    imageUrl: "https://source.unsplash.com/800x600/?bangkok",
+    category: "City Break",
+  },
+  {
+    city: "Sydney",
+    country: "Australia",
+    shortDescription:
+      "Experience the breathtaking beauty of Sydney, the Harbor City. Enjoy stunning views of the Opera House and Sydney Harbour Bridge while exploring this vibrant Australian metropolis.",
+    rating: 4.4,
+    price: {
+      amount: 1600,
+      currency: "A$",
+    },
+    imageUrl: "https://source.unsplash.com/800x600/?sydney",
+    category: "Scenic",
+  },
+  {
+    city: "Toronto",
+    country: "Canada",
+    shortDescription:
+      "Discover the dynamic city of Toronto, Canada's cultural and financial hub. Explore diverse neighborhoods, iconic landmarks, and world-class attractions.",
+    rating: 4.5,
+    price: {
+      amount: 1400,
+      currency: "C$",
+    },
+    imageUrl: "https://source.unsplash.com/800x600/?toronto",
+    category: "City Break",
+  },
+];
